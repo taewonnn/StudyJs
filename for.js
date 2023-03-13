eb21e45abc8b7adfa333438f587a8a39c0054dce
TaeWon Park <thisis_tw94@naver.com>
2023-03-13 21:51:51 +0900

    120887 / 120890 가까운 수 찾기 / 같은 문자열 비교

---
+++ for.js
@@ -0,0 +1,33 @@
+// 이중 For문
+let zzol = ['a', 'b', 'c', 'd', 'e'];
+let daram = ['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ'];
+
+for (let i = 0; i <zzol.length; i ++) {
+  for (let j =0; j < daram.length; j ++) {
+    console.log(zzol[i], daram[j])
+  }
+}
+
+
+/*
+a ㄱ
+a ㄴ
+a ㄷ
+a ㄹ
+b ㄱ
+b ㄴ
+b ㄷ
+b ㄹ
+c ㄱ
+c ㄴ
+c ㄷ
+c ㄹ
+d ㄱ
+d ㄴ
+d ㄷ
+d ㄹ
+e ㄱ
+e ㄴ
+e ㄷ
+e ㄹ
+*/
