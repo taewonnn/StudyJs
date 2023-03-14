4a80f283cd124ea39c3e9ca4b524cc9c6026817b
TaeWon Park <thisis_tw94@naver.com>
2023-03-14 20:47:02 +0900

    isNaN

---
+++ isNan.js
@@ -0,0 +1,10 @@
+// isNaN()
+//
+//
+// 숫자가 아닌 값을 찾는 함수!!
+// 숫자가 아니면(Not a Number) True를 반환 / 인수가 숫자이면 False를 반환
+
+console.log(isNaN("123+123")); // true
+
+console.log(isNaN(123)) // false
+console.log(!isNaN(123)) // true
