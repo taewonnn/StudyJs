9947edf89a9ce596c6883913c045daadccb34663
TaeWon Park <thisis_tw94@naver.com>
2023-03-17 21:22:32 +0900

    #2.4 styledComponent animation

---
+++ replace.js
@@ -0,0 +1,15 @@
+// replace()
+
+
+//  replace() 함수는
+// 문자열에서 변경하려는 문자열이 여러 번 반복될 경우, 첫 번째로 발견한 문자열만 치환해준다.
+
+
+// replaceAll()는 모든 문자열을 치환
+
+let zzinDda = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
+
+
+zzinDda.forEach((str, zzin) => {
+  numbers = numbers.replaceAll(str, zzin)
+})
