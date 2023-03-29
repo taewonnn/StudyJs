426edc99d666f578517941a144e3ed7690db522c
TaeWon Park <thisis_tw94@naver.com>
2023-03-29 20:08:42 +0900

    trim

---
+++ trim.js
@@ -0,0 +1,12 @@
+// trim
+
+
+// 앞 뒤 공백을 제거 해준다!
+
+const greeting = '   Hello world!   ';
+
+console.log(greeting);
+// Expected output: "   Hello world!   ";
+
+console.log(greeting.trim());
+// Expected output: "Hello world!";
