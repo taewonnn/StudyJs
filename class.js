0109b13c618a4146e415cd8505c7d8519fa7166d
TaeWon Park <thisis_tw94@naver.com>
2023-04-08 11:15:13 +0900

    class

---
+++ class.js
@@ -0,0 +1,20 @@
+// class
+
+class YalcoChicken {
+  constructor (name, no) {
+    this.name = name;
+    this.no = no;
+  }
+  introduce () { // 💡 메서드
+    return `안녕하세요, ${this.no}호 ${this.name}점입니다!`;
+  }
+}
+
+const chain1 = new YalcoChicken('판교', 3);
+const chain2 = new YalcoChicken('강남', 17);
+const chain3 = new YalcoChicken('제주', 24);
+
+console.log(chain1, chain1.introduce());
+console.log(chain2, chain2.introduce());
+console.log(chain3, chain3.introduce());
+
