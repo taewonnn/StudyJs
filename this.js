f7b9f7b3cbcdbb19cb8c23ab7c569d3d1177a6d0
TaeWon Park <thisis_tw94@naver.com>
2023-04-12 14:48:29 +0900

    this

--- this.js
+++ this.js
@@ -17,3 +17,44 @@ function func () {
 func();
 
 
+// 3. 객체 안에서의 this
+
+// a -  객체 리터럴 - 해당 객체를 가리킴
+const obj = {
+  x: 123,
+  getX: function () {
+    return this.x;
+  }
+}
+
+console.log(obj.getX());
+
+
+// b - 생성자 함수 - 생성될 인스턴스를 가리킴
+function Person (name, age) {
+  this.name = name;
+  this.age = age;
+  this.introduce = function  () {
+    return `저는 ${this.name}, ${this.age}세입니다.`
+  }
+}
+
+console.log(
+  new Person('홍길동', 20).introduce()
+);
+
+
+// c - 클래스 선언 - 생성될 인스턴스를 가리킴
+class YalcoChicken {
+  constructor (name, no) {
+    this.name = name;
+    this.no = no;
+  }
+  introduce () {
+    return `안녕하세요, ${this.no}호 ${this.name}점입니다!`;
+  }
+}
+
+console.log(
+  new YalcoChicken('강남', 17).introduce()
+);
