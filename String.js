4b11e7a7c064fa6844026d9df78472d1d5e5e6b5
TaeWon Park <thisis_tw94@naver.com>
2023-04-17 20:53:23 +0900

    string

---
+++ String.js
@@ -0,0 +1,39 @@
+// String
+
+
+
+
+
+
+
+// 이스케이프 표현	대체
+// \'	작은따옴표
+// \"	큰따옴표
+// \n	줄바꿈
+// \t	탭
+// \\	백슬래시
+
+let word = '안녕하세요~\t\t반갑습니다!\n저는 \\홍길동\\입니다.';
+console.log(word);
+
+
+
+// II. 백틱 - ` ~ `
+
+let word2 = `헬로헬로~ 🤩`;
+console.log(word2);
+
+
+const NAME = '홍길동';
+let age = 20;
+let married = false;
+
+console.log(
+  `제 이름은 ${NAME}, 나이는 ${age}세구요, \
+${married ? '기혼' : '미혼'}입니다.`
+);
+
+
+
+
+
