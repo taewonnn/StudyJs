869c041da40716fe6905b0347c56cc33469dc01d
TaeWon Park <thisis_tw94@naver.com>
2023-05-20 00:33:01 +0900

    class- ex

--- class.js
+++ class.js
@@ -100,3 +100,14 @@ const counter = new Counter(0);
 counter.increment(); // 1
 counter.increment(); // 2
 console.log(counter.value);
+
+
+
+
+
+// Quiz2
+// 정직원과 파트타임직원을 나타낼 수 있는 클래스를 만들어 보자
+// 직원들의 정보: 이름, 부서이름, 한달 근무 시간
+// 매달 직원들의 정보를 이용해서 한달 월급을 계산할 수 있다
+// 정직원은 시간당 10000원
+// 파트타임 직원은 시간당 8000원
