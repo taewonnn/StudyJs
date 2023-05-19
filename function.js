85114777ea8ad5afa61d50ac1a2fa385b4a13248
TaeWon Park <thisis_tw94@naver.com>
2023-05-19 23:43:20 +0900

    function - ex

--- function.js
+++ function.js
@@ -67,3 +67,24 @@ const mult = (x, y) => {
 };
 
 console.log(mult(2, 7));
+
+
+// quiz
+// 주어진 숫자 만큼 0부터 순회하는 함수
+// 순회하면서 주어진 특정한 일을 수행해야 함
+// 5, 순회하는 숫자를 다 출력하고 싶음
+// 5, 순회하는 숫자의 두배값을 다 출력하고 싶음
+// function iterate(max, action)
+
+
+// 주어진 숫자 만큼 0부터 순회하는 함수
+// 순회하면서 주어진 특정한 일을 수행해야 함
+// 5, 순회하는 숫자를 다 출력하고 싶음
+// 5, 순회하는 숫자의 두배값을 다 출력하고 싶음
+// function iterate(max, action)
+
+function iterate(max, action) {
+  for (let i = 0; i < max; i++) {
+    action(i);
+  }
+}
