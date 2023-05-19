54d7967fbb49f1bcd6bd4ae642b5d7b6b086009c
TaeWon Park <thisis_tw94@naver.com>
2023-05-19 23:43:59 +0900

    function - ex

--- function.js
+++ function.js
@@ -98,3 +98,7 @@ function doubleAndLog(num) {
 }
 iterate(3, (num) => console.log(num));
 iterate(3, (num) => console.log(num * 2));
+
+setTimeout(() => {
+  console.log('3초뒤 이 함수가 실행될거예요');
+}, 3000);
