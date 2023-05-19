d1e279ce24f87ff361f46c3208980f6ea531340d
TaeWon Park <thisis_tw94@naver.com>
2023-05-20 00:37:36 +0900

    for- ex

--- for_2.js
+++ for_2.js
@@ -92,3 +92,9 @@ const ids = "user1, user2, user3, user4";
 const array = ids.split(", ");
 console.log(array);
 
+
+// 3. 1초에 한번씩 시계를 (날짜포함) 출력해보자
+setInterval(() => {
+  const now = new Date();
+  console.log(now.toLocaleString("ko-KR"));
+}, 1000);
