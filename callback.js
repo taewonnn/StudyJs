62d10a68184693b908f09ac9999cb2be9a7926af
TaeWon Park <thisis_tw94@naver.com>
2023-05-21 16:51:44 +0900

    callback

--- callback.js
+++ callback.js
@@ -3,6 +3,21 @@
 // 함수 안에 파라미터로 들어가는 함수
 // 용도 : 순차적으로 실행하고 싶을 때 사용!
 
+document.querySelector('.button').addEventListener('click', function () {
+
+});
+
+// addEventListener는 함수인데, 함수 안에 또다른 함수를 넣는다! -> 콜백함수
+// 버튼을 클릭했을 때, 이걸 실행해주세요!  - 순차적으로 실행
+
+
+
+setTimeout(function() {}, 1000)
+// 1초 뒤에 실행해주세요!
+
+
+
+
 function a() {
   for (let i = 0; i < 1000000000000000; i++);
   return 1;
