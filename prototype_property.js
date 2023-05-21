0062d8ab6a489a375eb871dc640266ebc67df683
TaeWon Park <thisis_tw94@naver.com>
2023-05-21 22:55:43 +0900

    prototype

--- prototype_property.js
+++ prototype_property.js
@@ -51,3 +51,30 @@ Object.defineProperties(student, {
   },
 });
 console.log(student);
+
+
+// quiz
+// 클래스를 베이스로한 객체지향프로그래밍
+
+
+class Animal {
+  constructor(name, emoji) {
+    this.name = name;
+    this.emoji = emoji;
+  }
+  printName() {
+    console.log(`${this.name} ${this.emoji}`);
+  }
+}
+
+class Dog extends Animal {
+  play() {
+    console.log('같이 놀자옹!');
+  }
+}
+class Tiger extends Animal {
+  hunt() {
+    console.log(`사냥하자! ..🐇..`);
+  }
+}
+
