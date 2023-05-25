5a8b9768998839e39227bdea7521fbfff81fc0d8
TaeWon Park <thisis_tw94@naver.com>
2023-05-26 01:22:06 +0900

    Event

--- event.js
+++ event.js
@@ -11,3 +11,39 @@ document.querySelector('#button1')
   .addEventListener('click', () => {
     alert('ㅇㅇ 어서오고');
   });
+
+
+
+// 이벤트 리스너에서의 this
+// ♻️ 새로고침 후 실행
+document.querySelector('#button1')
+  .addEventListener('click', function (e) {
+    console.log(this);
+    console.log(e.target);
+    this.textContent = '클릭됨';
+  });
+
+
+// mouseenter, mouseleave 이벤트
+// 마우스 커서가 들어올/나갈 때
+
+const $button2 = document.querySelector('#button2');
+
+let timeout;
+let interval;
+let countdown;
+
+
+$button2.addEventListener('mouseenter', () => {
+  if (timeout) clearTimeout(timeout);
+
+  $button2
+    .firstElementChild
+    .textContent = '💣 치워라, 5초 준다';
+
+  timeout = setTimeout(() => {
+    $button2
+      .firstElementChild
+      .textContent = '🔥🔥🔥🔥🔥🔥🔥🔥🔥';
+  }, 5000);
+});
