a59966f036b0758fd1e38b7e246d7eb850a8585e
TaeWon Park <thisis_tw94@naver.com>
2023-05-26 01:21:28 +0900

    Event

---
+++ event.js
@@ -0,0 +1,13 @@
+// Event
+
+
+// addEventListener - EventTarget의 기능
+// 첫 번째 인자로 주어진 이름의 이벤트 발생시 두 번째 인자로 주어진 콜백함수 실행
+
+
+// click
+
+document.querySelector('#button1')
+  .addEventListener('click', () => {
+    alert('ㅇㅇ 어서오고');
+  });
