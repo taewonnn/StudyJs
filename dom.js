3f7ec85d5c225d70ed9ff570d70ba8012fa705dd
TaeWon Park <thisis_tw94@naver.com>
2023-05-26 01:18:41 +0900

    Dom

--- dom.js
+++ dom.js
@@ -5,3 +5,27 @@ console.log(
   document === window.document,
   document === globalThis.document
 );
+
+
+// document 내에 html 태그 요소를 선택하는 코드
+const html = document.querySelector('html');
+
+console.log(html); // DOM 형태로 출력
+
+
+function getNameAndChildren (node, indent) {
+  console.log(
+    '  '.repeat(indent), // 인덴트
+    node.nodeName, // 노드 이름
+
+    // ⭐ 아래를 차례로 로깅해볼 것
+    // node.children, // 이터러블임 확인
+    // Object.getPrototypeOf(node),
+  );
+
+  for (child of node.children) {
+    getNameAndChildren(child, indent + 1);
+  }
+}
+
+getNameAndChildren(html, 0);
