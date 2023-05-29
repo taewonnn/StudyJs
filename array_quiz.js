184431bdde845b71c7b0787be524eeacfa756c40
TaeWon Park <thisis_tw94@naver.com>
2023-05-29 21:54:19 +0900

    array-quiz

--- array_quiz.js
+++ array_quiz.js
@@ -12,3 +12,16 @@ function replace (array, from, to) {
 const array = ['🍌', '🍓', '🍇', '🍓'];
 const result = replace(array, '🍓', '🥝');
 console.log(result);
+
+
+
+// 퀴즈2:
+// 배열과 특정한 요소를 전달받아,
+// 배열안에 그 요소가 몇개나 있는지 카운트 하는 함수 만들기
+// input: [ '🍌', '🥝', '🍇', '🥝' ], '🥝'
+// output: 2
+
+
+function count(array, item) {
+  return array
+}
