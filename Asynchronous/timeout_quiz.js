c7929f611d9bff2bb2ce67945a6643783a53d4d1
TaeWon Park <thisis_tw94@naver.com>
2023-07-08 11:12:51 +0900

    timeout-quiz

--- Asynchronous/timeout_quiz.js
+++ Asynchronous/timeout_quiz.js
@@ -2,5 +2,8 @@
 // 단, seconds가 0보다 작으면 에러를 던진다!
 
 function runInDelay(callback, seconds) {
-  return
+  if(!seconds || seconds < 0) {
+    throw new Error('seconds는 0보다 커야 한다!')
+  }
+  setTimeout(callback, seconds * 1000);
 }
