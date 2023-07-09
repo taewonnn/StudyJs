dbc193d996b09d13a93e203cf61c9b2f526d5952
TaeWon Park <thisis_tw94@naver.com>
2023-07-09 22:57:17 +0900

    promise

---
+++ Asynchronous/promise.js
@@ -0,0 +1,12 @@
+// Promise
+// object 형식 / 끝난 이벤트(비동기적으로 수행한 결과값)를 알려주는 객체
+
+
+// Promise의 상태 종류
+// pending(일이 끝나지 않은 상태) fullfilled(성공) rejected(실패)
+
+
+// then, catch, finally
+// then -
+// catch -
+// finally -
