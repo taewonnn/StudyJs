4fedef334a7235f4947dffe9ba52efeacaadffd2
TaeWon Park <thisis_tw94@naver.com>
2023-07-22 11:18:12 +0900

    typescript

--- TypeScript.ts
+++ TypeScript.ts
@@ -172,5 +172,35 @@ const b: String = 'hell';
 
 
 
+{
+  /**
+   * Type Aliases
+   */
+  type Text = string;
+  const name: Text = 'ellie';
+  const address: Text = 'korea';
+  type Num = number;
+  type Student = {
+    name: string;
+    age: number;
+  };
+  const student: Student = {
+    name: 'ellie',
+    age: 12,
+  };
+
+  /**
+   * String Literal Types
+   */
+  type Name = 'name';
+  let ellieName: Name;
+  ellieName = 'name';
+  type JSON = 'json';
+  const json: JSON = 'json';
+
+  type Boal = true;
+}
+
+
 
 
