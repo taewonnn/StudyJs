8b9dc06486de7b6c8f119fc9f62e564a7bc1a8d9
TaeWon Park <thisis_tw94@naver.com>
2023-09-24 10:00:00 +0900

    dom

--- dom-2.js
+++ dom-2.js
@@ -1,32 +1,27 @@
 // dom 요소 선택
 
-
 // getElementsByTagName - 태그명으로 선택
-const $sections = document.getElementsByTagName('section');
-const $listItems = document.getElementsByTagName('li');
+const $sections = document.getElementsByTagName("section");
+const $listItems = document.getElementsByTagName("li");
 
 console.log($sections);
 console.log($listItems);
 
-
-
 // getElementsByClassName - 클래스명으로 선택
 // document에서 사용시 - Document의 메서드
-const $plants = document.getElementsByClassName('plant');
-const $soldOuts = document.getElementsByClassName('soldout');
+const $plants = document.getElementsByClassName("plant");
+const $soldOuts = document.getElementsByClassName("soldout");
 
 console.log($plants);
 console.log($soldOuts);
 
-
 // getElementById - 아이디로 선택
-document.getElementById('sweets');
-
+document.getElementById("sweets");
 
 // querySelector, querySelectorAll
-document.querySelector('section');
+document.querySelector("section");
 
 // 💡 NodeList 반환 (HTMLCollection보다 다양한 타입 담음)
-document.querySelectorAll('section');
+document.querySelectorAll("section");
 
-document.querySelector('.plant > ul > .soldout');
+document.querySelector(".plant > ul > .soldout");
