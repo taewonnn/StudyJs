378a3e094bba9f5a3f60ad0a0e9f589341566bb7
TaeWon Park <thisis_tw94@naver.com>
2023-09-25 21:16:34 +0900

    closure

--- closure.js
+++ closure.js
@@ -1,10 +1,10 @@
 //  클로저 closure
 // 내부 함수에서 외부 함수의 값에 접근할 수 있다는 개념 함수 중첩시
 
-function func1 () {
-  const word = 'Hello';
+function func1() {
+  const word = "Hello";
 
-  function func2 () {
+  function func2() {
     console.log(word);
   }
 
@@ -15,34 +15,24 @@ const logHello = func1();
 
 logHello();
 
-
 // logHello에는 func1 안의 함수인 func2가 반환되어 지정됨
 // func1의 실행이 끝났음에도 불구하고, 해당 스코프 내의 값이 살아 있음
 // func2와 func2가 선언된 환경(func1의 스코프)의 조합 - 클로저
 
-
-
-function createCounter (start) {
+function createCounter(start) {
   let num = start;
 
   return function () {
     console.log(++start);
     return start;
-  }
+  };
 }
 
 const count = createCounter(10);
 
 // 단지 값을 복사해서 갖는 것이 아니라, 해당 값이 저장되는 외부 환경 자체가 유지됨
 
-
-
-
-
-
-
-
-const text = 'hello';
+const text = "hello";
 function func() {
   console.log(text);
 }
@@ -57,7 +47,6 @@ function outer() {
 }
 const func1 = outer();
 func1();
-
 
 // 왜 ??
 
