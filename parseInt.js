686d01c487a056db9ddda9d9c235b004eb491a7c
TaeWon Park <thisis_tw94@naver.com>
2024-03-22 10:00:00 +0900

    parseInt

--- parseInt.js
+++ parseInt.js
@@ -2,8 +2,6 @@ parseInt();
 
 // 정수를 반환 ← 주로 사용하던 기능
 // 문자열을 파싱 하여 특정 진수의 정수를 반환 (= 특정 진수를 10진수로)
-
-
 const str = "10";
 console.log(parseInt(str)); //10 (str을 정수로)
 console.log(parseInt(str, 2)); //2 (2진수인 str을 10진수로)
