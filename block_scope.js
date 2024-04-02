9cfb3892504da8302a6c403855d3155c89d1bf57
TaeWon Park <thisis_tw94@naver.com>
2024-04-02 22:03:34 +0900

    scope

--- block_scope.js
+++ block_scope.js
@@ -2,7 +2,6 @@
 
 
 // 블록문
-
 {
   console.log('블록문');
 }
@@ -13,32 +12,30 @@
 
 
 // 스코프
-
 // 블록 안에 선언된 변수와 상수를 밖에서 사용 불가
-let x = 1;
+let x1 = 1;
 
 {
   let y = 2;
   console.log(x, y);
 }
-console.log(x);
+console.log(x1);
 console.log(y);
 
 
 // 블록 안쪽에서는 바깥의 것 사용 가능
-let x = 1;
+let x2 = 1;
 
 {
   let y = 2;
   console.log(x, y);
 }
-console.log(x);
+console.log(x2);
 console.log(y);
 
 
 
 // 스코프 체인 scope chain
-
 // 🥞 스택 stack
 // 후입선출 - 나중에 온 것이 먼저 나감
 
