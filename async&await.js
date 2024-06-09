503a60332c099200021573bd7b7a19b604c01708
TaeWon Park <thisis_tw94@naver.com>
2024-06-09 20:23:24 +0900

    async

--- async&await.js
+++ async&await.js
@@ -1,8 +1,6 @@
 // async / await
-
 // 프로미스를 기반으로 동작
 // 마치 동기 코드처럼 직관적으로 코딩을 할 수 있음
-
 
 async function fetchFruits() {
   const banana = await getBanana();
@@ -48,4 +46,3 @@ console.log('💡 이 문구가 먼저 출력됨');
 
 // ⭐ await - 코드의 진행을 멈추고 프로미스로부터 답을 받아냄
 // 💡 await은 async 함수 또는 모듈 내에서만 사용 가능
-// 👉 MDN 문서 보기
