e94931c47c11936509c68c68646b058bb237ccac
TaeWon Park <thisis_tw94@naver.com>
2024-06-16 19:48:26 +0900

    lastIndexOf

--- lastIndexOf.js
+++ lastIndexOf.js
@@ -1,19 +1,19 @@
 lastIndexOf()
 
 
-indexOf는
-주어진 문자열 str 앞에서부터 (index: 0) 지정한 searchValue가 처음으로 발견되는 위치의 index 반환한다.
+//indexOf는
+//주어진 문자열 str 앞에서부터 (index: 0) 지정한 searchValue가 처음으로 발견되는 위치의 index 반환한다.
 
 
 let daram = 'abcdsvdsdfsdfe'
 
 daram.indexOf('d') // 3
 
-d가 여러 개 있지만, 순서대로 가서 맨 앞에 나온 d의 위치를 찾아준다.
+//d가 여러 개 있지만, 순서대로 가서 맨 앞에 나온 d의 위치를 찾아준다.
 
 
 
-lsatIndexOf()는 뒤에서부터 찾아준다!
+//lsatIndexOf()는 뒤에서부터 찾아준다!
 
 let zzoldaram = 'abcdsvdsdfsdfe'
 
