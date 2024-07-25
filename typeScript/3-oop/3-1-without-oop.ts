9b65e9896e0fb96b87a247e92b0981fd731a468a
TaeWon Park <thisis_tw94@naver.com>
2024-07-25 10:00:00 +0900

    ts

--- typeScript/3-oop/3-1-without-oop.ts
+++ typeScript/3-oop/3-1-without-oop.ts
@@ -5,8 +5,8 @@
   };
 
   const BEANS_GRAMM_PER_SHOT: number = 7;
-
   let coffeeBeans: number = 0;
+
   function makeCoffee(shots: number): CoffeeCup {
     if (coffeeBeans < shots * BEANS_GRAMM_PER_SHOT) {
       throw new Error('Not enough coffee beans!');
