ee4e06f53b5d44e98b2e1e3df61a9decebdfc345
TaeWon Park <thisis_tw94@naver.com>
2024-08-04 20:45:22 +0900

    ts

--- typeScript/5-generic/5-3-constrains.ts
+++ typeScript/5-generic/5-3-constrains.ts
@@ -48,6 +48,7 @@ console.log(getValue(obj, 'name')); // ellie
 console.log(getValue(obj, 'age')); // 20
 console.log(getValue(obj2, 'animal')); // 🐕
 
+
 function getValue<T, K extends keyof T>(obj: T, key: K): T[K] {
   return obj[key];
 }
