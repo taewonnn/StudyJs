ddc70422acae09fa8ef87af462d794ce91322a60
taewon <112356419+taewonnn@users.noreply.github.com>
2024-11-11 11:54:54 +0900

    1-3-array

--- typeScript/1-types/1-3-array.ts
+++ typeScript/1-types/1-3-array.ts
@@ -1,12 +1,15 @@
 {
   // Array
   const fruits: string[] = ['🍅', '🍌'];
+  const frunuts1: Array<string> = ['🍅', '🍌'];
   const scroes1: number[] = [1, 3, 4];
   const scroes: Array<number> = [1, 3, 4];
-  // 변경 불가 fruits 배열은 변경 불가(추가포함)
+
+  // 변경 불가 fruits 배열은 변경 불가(추가포함) => readonly 추가
   function printArray(fruits: readonly string[]) {}
 
-  // Tuple -> interface, type alias, class  - 서로다른 타입의 데이터를 담을 수 있음
+  // Tuple 서로다른 타입의 데이터를 담을 수 있는 배열 
+  // -> interface, type alias, class  - 서로다른 타입의 데이터를 담을 수 있음
   let student: [string, number];
   student = ['name', 123];
   student[0]; // name
