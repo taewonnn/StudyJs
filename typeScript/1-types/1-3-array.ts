536eb4415e29e45d045ebb770eca032057104dea
taewon <112356419+taewonnn@users.noreply.github.com>
2024-11-11 11:56:25 +0900

    1-3-array

--- typeScript/1-types/1-3-array.ts
+++ typeScript/1-types/1-3-array.ts
@@ -8,7 +8,7 @@
   // 변경 불가 fruits 배열은 변경 불가(추가포함) => readonly 추가
   function printArray(fruits: readonly string[]) {}
 
-  // Tuple 서로다른 타입의 데이터를 담을 수 있는 배열 
+  // Tuple 서로다른 타입의 데이터를 담을 수 있는 배열 -> 사용하지말고 아래 interface / type / alias로 사용하는 것 권장
   // -> interface, type alias, class  - 서로다른 타입의 데이터를 담을 수 있음
   let student: [string, number];
   student = ['name', 123];
