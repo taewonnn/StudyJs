2ee6f98d3d06467142118ca29bfa314dffcb7112
taewon <112356419+taewonnn@users.noreply.github.com>
2024-11-19 16:46:25 +0900

    enum

--- typeScript/1-types/1-8-enum.ts
+++ typeScript/1-types/1-8-enum.ts
@@ -1,8 +1,8 @@
 {
   /**
-   * Enum
+   * Enum -> 여러가지의 관련된 상수값
    */
-  // JavaScript
+  // JavaScript에서는 const로 사용
   const MAX_NUM = 6;
   const MAX_STUDENTS_PER_CLASS = 10;
   const MONDAY = 0;
@@ -14,15 +14,17 @@
   // TypeScript
   type DaysOfWeek = 'Monday' | 'Tuesday' | 'Wednesday';
   enum Days {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday,
+    Monday, // 0
+    Tuesday, // 1
+    Wednesday, // 2
+    Thursday, // 3
+    Friday, // 4
+    Saturday, // 5
+    Sunday, //6
   }
-  console.log(Days.Monday);
+  
+  console.log(Days.Monday);  // 0
+  
   let day: Days = Days.Saturday;
   day = Days.Tuesday;
   day = 10;
