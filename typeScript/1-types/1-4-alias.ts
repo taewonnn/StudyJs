cb7a9fef31cc816e991202ec38065c385ffc03d2
taewon <112356419+taewonnn@users.noreply.github.com>
2024-11-19 15:53:03 +0900

    alias

--- typeScript/1-types/1-4-alias.ts
+++ typeScript/1-types/1-4-alias.ts
@@ -1,17 +1,19 @@
 {
   /**
    * Type Aliases
+   * 새로운 type을 내가 정할 수 있다!
    */
-  type Text = string;
+  type Text = string; // Text라는 타입은 문자열 타입이다!
   const name: Text = 'ellie';
-  
   const address: Text = 'korea';
   type Num = number;
-  
+
+  // Obj에서도 가능!
   type Student = {
     name: string;
     age: number;
   };
+  
   const student: Student = {
     name: 'ellie',
     age: 12,
