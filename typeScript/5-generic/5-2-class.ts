06caa7b54ffd51857af2a170b7a1a8aa63986048
taewon <112356419+taewonnn@users.noreply.github.com>
2024-11-25 11:27:48 +0900

    class

--- typeScript/5-generic/5-2-class.ts
+++ typeScript/5-generic/5-2-class.ts
@@ -1,4 +1,6 @@
 // either: a or b
+// L,R 두 개 타입을 같은 타입을 쓸 수도 다른 타입을 쓸 수 도 있음
+// generic 타입은 보통  대문자 한글자로만 씀
 interface Either<L, R> {
   left: () => L;
   right: () => R;
@@ -17,6 +19,8 @@ class SimpleEither<L, R> implements Either<L, R> {
 
 
 const either: Either<number, number> = new SimpleEither(4, 5);
+
 either.left(); // 4
 either.right(); //5
-const best = new SimpleEither({name: 'ellie'}, 'hello');
+
+const best = new SimpleEither({name: 'ellie'}, 'hello'); // ellie hello
