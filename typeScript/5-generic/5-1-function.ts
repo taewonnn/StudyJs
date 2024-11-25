741057613e4e1c573583a12648474dc59cb1f811
taewon <112356419+taewonnn@users.noreply.github.com>
2024-11-25 11:17:31 +0900

    function

--- typeScript/5-generic/5-1-function.ts
+++ typeScript/5-generic/5-1-function.ts
@@ -1,13 +1,26 @@
+// generic
+
+
 {
+
+  // item이 null인지 확인하는 함수 
   function checkNotNullBad(arg: number | null): number {
     if (arg == null) {
       throw new Error('not valid number!');
     }
+
+    // null이 아닌 경우 arg 리턴
     return arg;
   }
 
+  console.log(checkNotNullBad(123)) // 123
+  console.log(checkNotNullBad(null)) // not valid number!
 
   
+
+
+
+  // item이 어떤 type인지 모두 확인해주는 함수
   function checkNotNullAnyBad(arg: any | null): any {
     if (arg == null) {
       throw new Error('not valid number!');
@@ -25,3 +38,6 @@
   const number = checkNotNull(123);
   const boal: boolean = checkNotNull(true);
 }
+
+
+
