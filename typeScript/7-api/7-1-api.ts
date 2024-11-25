49354afe91219fe608a2cf0178403c67cde6cde0
taewon <112356419+taewonnn@users.noreply.github.com>
2024-11-25 13:49:33 +0900

    api

--- typeScript/7-api/7-1-api.ts
+++ typeScript/7-api/7-1-api.ts
@@ -10,7 +10,7 @@ type Student {
   passed: boolean;
 }
 
-cosnt students: Student[] = [{passed: true},{passed: true},{passed: false}];
+const students: Student[] = [{passed: true},{passed: true},{passed: false}];
 
 const result = students.every(student => student.passed);
 
