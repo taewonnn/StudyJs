d62bdcec87d0ab2ccf00139a2d8c259945e66221
TaeWon Park <thisis_tw94@naver.com>
2024-11-29 10:00:00 +0900

    function

--- typeScript/5-generic/5-1-function.ts
+++ typeScript/5-generic/5-1-function.ts
@@ -16,9 +16,6 @@
   console.log(checkNotNullBad(123)) // 123
   console.log(checkNotNullBad(null)) // not valid number!
 
-  
-
-
 
   // item이 어떤 type인지 모두 확인해주는 함수(any라 안전하지 않음)
   function checkNotNullAnyBad(arg: any | null): any {
