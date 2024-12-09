aa871a1cc47a7e8cb190a1e7e5bf39e6b78e651c
taewon <112356419+taewonnn@users.noreply.github.com>
2024-12-09 15:24:13 +0900

    index

---
+++ typeScript/9-advanced/index.ts
@@ -0,0 +1,33 @@
+{
+  const obj = {
+    name: 'ellie',
+  };
+  obj.name; // ellie
+  obj['name']; // ellie
+
+  type Animal = {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+  };
+
+  type Name = Animal['name']; // Name의 타입은 string
+  const text: Name = 'hello';
+
+  type Gender = Animal['gender']; //Gender의 타입은 'male' | 'female'
+
+  // keyof -> 타입의 키를 ~
+  // Animal의 모든 키의 타입을
+  type Keys = keyof Animal; // 'name' | 'age' | 'gender'
+  const key: Keys = 'gender';
+
+  type Person = {
+    name: string;
+    gender: Animal['gender']; // 'male' 또는 'female'
+  };
+
+  const person: Person = {
+    name: 'ellie',
+    gender: 'male',
+  };
+}
