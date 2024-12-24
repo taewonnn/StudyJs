cc288dce47c1ce91118c0d460f573af4de67690f
TaeWon Park <thisis_tw94@naver.com>
2024-12-24 10:00:00 +0900

    dom-2

--- dom-2.js
+++ dom-2.js
@@ -7,6 +7,7 @@ const $listItems = document.getElementsByTagName("li");
 console.log($sections);
 console.log($listItems);
 
+
 // getElementsByClassName - 클래스명으로 선택
 // document에서 사용시 - Document의 메서드
 const $plants = document.getElementsByClassName("plant");
