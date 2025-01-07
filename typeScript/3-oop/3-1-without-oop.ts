b3bf05a5766d8360ca4d979662331196016ad419
eden <eden@rainbow.co.kr>
2025-01-07 18:25:48 +0900

    feat: oop

--- typeScript/3-oop/3-1-without-oop.ts
+++ typeScript/3-oop/3-1-without-oop.ts
@@ -1,29 +1,27 @@
 {
-  type CoffeeCup = {
+  type CofeeCup = {
     shots: number;
-    hasMilk: boolean;
-  };
-
-
-
-  const BEANS_GRAMM_PER_SHOT: number = 7;
-  let coffeeBeans: number = 0;
-
-  function makeCoffee(shots: number): CoffeeCup {
-    // coffeeBeans가 shots보다 적을 때는 커피 못만드니 에러
-    if (coffeeBeans < shots * BEANS_GRAMM_PER_SHOT) {
-      throw new Error('Not enough coffee beans!');
-    }
-    coffeeBeans -= shots * BEANS_GRAMM_PER_SHOT;
-    return {
-      shots,
-      hasMilk: false,
-    };
+    hasMilk:boolean;
   }
 
-  
+  let BEANS_GRAMM_PER_SHOT:number = 1; // 1샷 당 커피 중량
+  let coffeeBeans:number = 0; // 커피콩
 
-  coffeeBeans += 3 * BEANS_GRAMM_PER_SHOT;
+  function makeCoffee(shots: number): CofeeCup {
+    // coffeeBeans가 shots보다 적을 때는 커피 못만드니 에러
+    if(coffeeBeans < shots * BEANS_GRAMM_PER_SHOT) {
+      throw new Error('Not Enough Coffee beans!')
+    } 
+
+    coffeeBeans -= shots * BEANS_GRAMM_PER_SHOT; // 사용한 만큼 커피콩 줄임
+
+    return {
+      shots: shots,
+      hasMilk:false
+    } 
+  }
+
+  coffeeBeans+=4;
   const coffee = makeCoffee(2);
   console.log(coffee);
 }
